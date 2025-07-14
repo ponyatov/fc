a0bf308b9d5e50c793904bac9bf9701c7205654d
Dmitry Ponyatov <dponyatov@gmail.com>
2025-07-14 15:11:36 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- inc/fc.hpp
+++ inc/fc.hpp
@@ -1,12 +1,26 @@
 #pragma once
 
+/// @defgroup libc libc
+/// @{
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+/// @}
 
+/// @defgroup main main
+/// @ingroup libc
+/// @{
 extern int main(int argc, char *argv[]);
 extern void arg(int argc, char *argv);
+/// @}
 
+/// @defgroup flang F
+/// @brief tiny language for embedded devices (Cortex-M0..7, RISC-V, Xtensa)
+
+/// @defgroup parser parser
+/// @brief syntax parser
+/// @ingroup flang
+/// @{
 extern int yylex();
 extern int yylineno;
 extern char *yytext;
@@ -14,3 +28,5 @@ extern char *yyfile;
 extern FILE *yyin;
 extern int yyparse();
 extern void yyerror(char *msg);
+#include "fc.yacc.hpp"
+/// @}
